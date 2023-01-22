88927bfd1824562ced6e1e209fcd083c0f6b4782
wahyud1n <73865876+wahyud1n@users.noreply.github.com>
2023-01-22 12:23:21 +0800

    Add files via upload

---
+++ partials/footer.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      <b>Version</b> 0.1.0
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2022 <a href="#">Praktikum Web 2</a>.</strong> All rights reserved.
+  </footer>
